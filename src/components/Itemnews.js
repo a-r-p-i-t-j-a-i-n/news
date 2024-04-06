8362e46a7272eb445b9d0ec34116e32934fc9ec1
a-r-p-i-t-j-a-i-n <arpitjian@gmail.com>
2024-04-07 01:03:16 +0530

    second

--- src/components/Itemnews.js
+++ src/components/Itemnews.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class Itemnews extends Component {
   render() {
-    let {title,description,Imageurl}=this.props
+    let { title, description, Imageurl } = this.props;
     return (
-        <div className="card" style={{width: "18rem"}}>
-        <img src={Imageurl} className="card-img-top" alt="..."/>
-        <div className="card-body">
+      <div className="card" style={{ width: "18rem", marginBottom: "20px" }}>
+        <img src={Imageurl} className="card-img-top" alt="..." />
+        <div className="card-body" style={{ maxHeight: "200px", overflow: "hidden" }}>
           <h5 className="card-title">{title}</h5>
-          <p className="card-text">{description}</p>
-          <a href="url" className="btn btn-primary">Read more</a>
+          <p className="card-text" style={{ marginBottom: "0" }}>{description}</p>
+          <a href="url" className="btn btn-primary" style={{ position: "absolute", bottom: "10px" }}>Read more</a>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Itemnews
+export default Itemnews;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class Navbar extends Component {
   render() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div className="container-fluid">
           <a className="navbar-brand" href="hero">Navbar</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="heronavbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,21 +15,18 @@ export class Navbar extends Component {
                 <a className="nav-link active" aria-current="page" href="hero">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="hero">catagory</a>
+                <a className="nav-link" href="hero">Category</a>
               </li>
-              
             </ul>
             <form className="d-flex">
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
               <button className="btn btn-outline-success" type="submit">Search</button>
             </form>
-            
           </div>
         </div>
       </nav>
-    )
+    );
   }
 }
 
-
-export default Navbar
+export default Navbar;
